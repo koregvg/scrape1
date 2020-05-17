37c444c7f2a433a1cf9c2f16c1db075a43421431
王派 <wangpai@xiaomi.com>
2020-05-17 17:24:45 +0800

    'first'

--- server/server.js
+++ server/server.js
@@ -94,18 +94,45 @@
 // }
 // console.log('This websocket-server is running at localhost:' + 11881)
 
-const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({
-  port: 11881
-});
 
-wss.on('connection', function connection(ws, req) {
-  const ip = req.socket.remoteAddress;
-  console.log(req.socket)
-  console.log(ip)
-});
 
-console.log('This websocket-server is running at localhost:' + 11881)
+
+
+// const WebSocket = require("ws");
+//
+// const wss = new WebSocket.Server({
+//   port: 11881
+// });
+//
+// wss.on('connection', function connection(ws, req) {
+//   const ip = req.socket.remoteAddress;
+//   console.log(req.socket)
+//   console.log(ip)
+// });
+//
+// console.log('This websocket-server is running at localhost:' + 11881)
+
+var dgram = require('dgram');
+//创建 udp server
+var udp_server = dgram.createSocket('udp4');
+udp_server.bind(11881); // 绑定端口
+
+// 监听端口
+udp_server.on('listening', function () {
+  console.log('udp server linstening 11881.');
+})
+
+//接收消息
+udp_server.on('message', function (msg, rinfo) {
+  strmsg = msg.toString();
+  udp_server.send(strmsg, 0, strmsg.length, rinfo.port, rinfo.address); //将接收到的消息返回给客户端
+  console.log(`udp server received data: ${strmsg} from ${rinfo.address}:${rinfo.port}`)
+})
+//错误处理
+udp_server.on('error', function (err) {
+  console.log('some error on udp server.')
+  udp_server.close();
+})
 
 
